f90665bd640e9c13b6655e79bb396bc01271129f
dbh937 <dbh937@gmail.com>
2013-02-16 22:54:43 -0400

    implemented about. and help. commands

--- notepad.js
+++ notepad.js
@@ -1,41 +1,48 @@
-function current_line(textarea) {
-  var ta = document.getElementById(textarea),
-      pos = ta.selectionStart;
-      taval = ta.value;
-      start = taval.lastIndexOf('\n', pos - 1) + 1,
-      end = taval.indexOf('\n', pos);
-  if (end == -1)
-    end = taval.length;
-  return taval.substr(start, end - start);
-}
 
 /*
   * Some conventions:
     * When a textarea is being passed into a function, it's always passed in as the string
     * of the area's id.
-    * Textarea is always last argument of the function.
 */
 
-var commandFuncs = {
+var funcs = {
   "save": {
+    "refresh": function(ta) {
+      this.update(ta);
+      this.redraw(ta);
+    },
     "update": function(ta) {
-        doc = document.getElementById(ta).value.split("\n");
-        document.getElementById.value = doc.join('\n');
+      doc = document.getElementById(ta).value.split("\n");
+    },
+    "redraw": function(ta) {
+      document.getElementById(ta).value = doc.join('\n');
     }
   },
   "util": {
-    "insertAtLine": function(text, command, ta) {
+    "isTempDoc": false, 
+    "insertAtLine": function(text, ta, c, useTempDoc) {
+      removeCommand(c, ta);
+      funcs.save.update(ta);
+      this.isTempDoc = useTempDoc;
       var lineNum = this.findLineNumber(ta);
       var taval = document.getElementById(ta).value;
       var whatToInsert = text.split('\n');
 
-      // All this tempDoc shit is a horrible solution, just couldn't think of anything better.
-      var tempDoc = taval.split("\n");
 
-      for (var i = 0; i < whatToInsert.length; i++) {
-        tempDoc.splice(i + lineNum, 0, whatToInsert[i]);
+      // All this tempDoc shit is a horrible solution, just couldn't think of anything better.
+      if (useTempDoc) {
+        var tempDoc = taval.split("\n");
+
+        for (var i = 0; i < whatToInsert.length; i++) {
+          tempDoc.splice(i + lineNum, 0, whatToInsert[i]);
+        }
+        document.getElementById(ta).value = tempDoc.join("\n");
+      } else {
+        for (var i = 0; i < whatToInsert.length; i++) {
+          doc.splice(i + lineNum, 0, whatToInsert[i]);
+        }
+        funcs.save.redraw("txt");
       }
-      document.getElementById(ta).value = tempDoc.join("\n");
     },
     "findLineNumber": function(ta) {
       var taval = document.getElementById(ta);
@@ -45,14 +52,31 @@ var commandFuncs = {
           lineCount++;
       }
       return lineCount;
+    },
+    "current_line": function(textarea) {
+      var ta = document.getElementById(textarea),
+        pos = ta.selectionStart;
+        taval = ta.value;
+        start = taval.lastIndexOf('\n', pos - 1) + 1,
+        end = taval.indexOf('\n', pos);
+      if (end == -1)
+        end = taval.length;
+      return taval.substr(start, end - start);
     }
   },
   "ajax": {
     "send": function(data) {
       console.log(data); // TODO: write backend code
+      if(data.command == "list")
+        return ["doc1", "doc 2", "doc three"];
     }
   }
 };
+
+var strs = {
+  "help": "here are the commands:\n - login: [username] [pasword]\n\t- lets you login to the service.\n- list: [tag]\n\t- List all documents accessable to you on SimplestNote, use the 'all' tag for all documents.\n- this.\n\t- Type this command on the line of the file you want to open after calling list, and the file will be opened.\n- title: [title]\n\t- adds a title to the document.\n- tag: [tags]\n\t- adds tags to a document.\n- save.\n\t- Saves the document.\n- logout.\n\t- pretty self-explanatory.\n- help.\n\t- Shows the help for all the commands.\n- about.\n\t- Don't really have to explain that one either.\npress space to remove the help.",
+  "about": "SimplestNote\n------------\ntitle. tag. save. pretty simple.\n\nSimplestNote was created by 14 year old Davis Haupt. You can check the project out on github[1], and if you really like it, you can donate[2]!\n[1]: https://github.com/dbh937/SimplestNote\n[2]: insert paypal here.\npress space to make this go away."
+}
 
 var doc = [];
 
@@ -62,35 +86,39 @@ var TA = document.getElementById("txt");
 
   TA.onkeypress = function(e) {
     if (e.which == 13) {
-      var found = current_line('txt').match(argRegEx);
+      var found = funcs.util.current_line('txt').match(argRegEx);
       if (found != null) {
-        commandFuncs.ajax.send({"command": found[1], "data": found[2].split(" ")});
+        funcs.ajax.send({"command": found[1], "data": found[2].split(" ")});
       }
+
     }
   };
   TA.onkeyup = function(e) {
-    var word = current_line('txt');
+    var word = funcs.util.current_line('txt');
     // period
     if (e.which == 190) {
       if (word == "save.") {
         removeCommand(word, "txt");
-        commandFuncs.save.update("txt");
-        commandFuncs.ajax.send({"command": "save", "data": doc});
+        funcs.save.refresh("txt");
+        funcs.ajax.send({"command": "save", "data": doc});
       } else if (word == "logout.") {
-        commandFuncs.ajax.send("logged out");
+        funcs.ajax.send("logged out");
       } else if (word == "this.") {
-        ;
+        console.log("this!");
       } else if (word == "help.") {
-        commandFuncs.util.insertAtLine("This will be the help\nhelpint is my job", "txt");
+        funcs.util.insertAtLine(strs.help, "txt", word, true);
       } else if (word == "about.") {
-        commandFuncs.util.insertAtLine("about dbh937\nhe is cool.", "txt");
+        funcs.util.insertAtLine(strs.about, "txt", word, true);
       } else if (word == "doc.") {
         console.log(doc);
         removeCommand(word, "txt");
       } else if (word == "clear.") {
         document.getElementById("txt").value = "";
-        commandFuncs.save.update("txt");
+        funcs.save.refresh("txt");
       }
+    } else if (e.which == 32 && funcs.util.isTempDoc) {
+      funcs.save.redraw("txt");
+      funcs.util.isTempDoc = false;
     } 
 
   };
