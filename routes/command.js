980459b474bb21acfc6d9ba951467d93a0b2487c
dbh937 <dbh937@gmail.com>
2013-08-13 15:40:11 -0400

    documents can now be loaded from the server

--- routes/command.js
+++ routes/command.js
@@ -11,17 +11,17 @@ var docSchema = new mongoose.Schema({
 });
 
 var Document = mongoose.model('Document', docSchema);
-
 exports.post = function (req, res) {
-  var ajaxResponse = {"command": req.body.command, "todo": ''};
   switch(req.body.command) {
     case "login":
       break;
     case "signup":
       break;
     case "list":
+      getListOfDocs(req.body.data, res, "");
       break;
     case "load":
+      loadDoc(req.body.data, res, "");
       break;
     case "title":
       // N/a
@@ -30,7 +30,7 @@ exports.post = function (req, res) {
       // Nothing should go here...
       break;
     case "save":
-        createOrEditDoc(req.body.data, res); // Create a new document or just update an existing one
+        createOrEditDoc(req.body.data, res, ""); // Create a new document or just update an existing one
       break;
     case "logout":
       break;
@@ -39,13 +39,54 @@ exports.post = function (req, res) {
  
 };
 
+
+// ajaxResponse = {"command": req.body.command, "todo": [text to insert], "doc": [new/updated document]};
 function sendToClient(res, ajaxResponse) {
   res.set("Content-Type", "application/json");
   res.send(JSON.stringify(ajaxResponse));
 }
 
 
-function createOrEditDoc(data, res) {
+function loadDoc(f, res, user) {
+
+  mongoose.connect("mongodb://localhost/test");
+  var db = mongoose.connection;
+
+  db.once('open', function() {
+    Document.findOne({"title": f, "user": ""}, function(err, doc) {
+      if (!err) {
+        sendToClient(res, {"command": "load", "todo": "", "doc": {"id": doc._id, 
+                                                                "title": doc.title, 
+                                                                "tag": doc.tag, 
+                                                                "content": doc.content}
+                                                              });
+      }
+
+
+      mongoose.disconnect();
+    });
+  });
+
+}
+
+function getListOfDocs(data, res, user) {
+  mongoose.connect('mongodb://localhost/test');
+  var db = mongoose.connection;
+  db.once('open', function() {
+    Document.find({"user": user}, function(err, docs) {
+      var l = []
+      for (var i = 0; i < docs.length; i++) {
+        l[i] = docs[i].title;
+      }
+      sendToClient(res, {"command": "list", "todo": l.join("\n")});
+
+      mongoose.disconnect();
+    });
+  });
+}
+
+
+function createOrEditDoc(data, res, user) {
   // takes the ID of a document and puts in the data, but only if it's different.
   // if `id` is null, then it creates a document.
   mongoose.connect('mongodb://localhost/test');
@@ -57,7 +98,6 @@ function createOrEditDoc(data, res) {
     if (data.id) { // if the document exists
       Document.findByIdAndUpdate(data.id, data, function(err, doc) {
         if (!err) {
-          console.log("updated!");
           mongoose.disconnect(function() {
             sendToClient(res, {"command": "save", "todo": "", "doc": data});
           });
@@ -66,6 +106,7 @@ function createOrEditDoc(data, res) {
 
     }
     else {
+      data.user = user;
       doc = new Document(data); // for now it's the same.
       doc.save(function(err) {
         if (!err) {
